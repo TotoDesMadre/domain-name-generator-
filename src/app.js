00bd16276f403a60301265fa85535d9f9046f56b
TotoDesMadre <jinqiaoxincun@outlook.com>
2021-11-02 23:31:22 +0000

    completed assignment

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  const array3 = ["$", "/", "?"];
+
+  // ['A1', 'A2', 'A3', 'B1', ..., 'C3']
+
+  const array = [];
+
+  // All possible Combinations
+
+  for (let i = 0; i < pronoun.length; i += 1) {
+    for (let j = 0; j < adj.length; j += 1) {
+      for (let k = 0; k < noun.length; k += 1) {
+        const a = pronoun[i];
+        const b = adj[j];
+        const c = noun[k];
+        array.push(a + b + c);
+      }
+    }
+  }
+
+  console.log(array);
 };
